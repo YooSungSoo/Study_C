7ff6b8367d3b3dd989593ab0ccb1788bf8cba558
SungSoo Yoo <elcane2@ajou.ac.kr>
2023-07-18 17:48:43 +0900

    Basic

--- Practice/p4.c
+++ Practice/p4.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 
+int A(int a)
+{
+    int num=0;
+    for(int i=1;i<=a;i++)
+    {
+        if(a%i==0)
+            num++;
+    }
+    if(num==2)
+            printf("%d ",a);
+}
+
 int main(void)
 {
-    
-
+    for(int i=1;i<100;i++)
+        A(i);
+ 
     return 0;
 }
 
